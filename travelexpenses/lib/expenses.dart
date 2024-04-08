3ab2a7497c4a522137c68cb915c6d4e099d158dc
Jenkibear <jenkinwork123@gmail.com>
2024-04-08 17:56:17 +1000

    enum created data model inital dummy data

--- travelexpenses/lib/expenses.dart
+++ travelexpenses/lib/expenses.dart
@@ -1,21 +1,39 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:travelexpenses/models/expense.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses();
 
   @override
-  State<Expenses> createState(){
+  State<Expenses> createState() {
     return _Expenses();
   }
 }
 
-class _Expenses extends State<Expenses>{
+class _Expenses extends State<Expenses> {
+  List<Expense> _myExpenses = [
+    Expense(
+        title: 'dinner',
+        amount: 20,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'sky diving',
+        amount: 40,
+        date: DateTime.now(),
+        category: Category.experience)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("Travel Expenses"),
+      body: Column(
+        children: [
+          Text("Travel Expenses"),
+          Text("Travel chart"),
+          Text("Travel list"),
+        ],
+      ),
     );
   }
 }
---
+++ travelexpenses/lib/models/expense.dart
@@ -0,0 +1,27 @@
+import 'package:uuid/uuid.dart';
+
+
+
+enum Category {food,experience,shopping,stay}
+
+const uuid=Uuid();
+class Expense{
+
+  
+  
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+
+  }):id=uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  
+}
