fd26116c3643f2685fe55abfedf11b8212e1033c
Jenkibear <jenkinwork123@gmail.com>
2024-04-09 08:25:03 +1000

    design modal and select date

--- travelexpenses/lib/widgets/new_expense.dart
+++ travelexpenses/lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,12 +13,12 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  
-  final _titleController =TextEditingController();
-
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
   @override
-  void dispose(){
+  void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -34,12 +36,41 @@ class _NewExpenseState extends State<NewExpense> {
             ),
           ),
           Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      prefixText: '\$',
+                      label: Text("Amount"),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                  Text("Select date"),
+                  IconButton(onPressed: (){}, icon: const Icon(Icons.calendar_month))
+                ],)
+              ],
+            ),
+          const SizedBox(height: 30,),
+          Row(
             children: [
               ElevatedButton(
                   onPressed: () {
                     print(_titleController.text);
+                    print(_amountController.text);
                   },
-                  child: const Text("Save Expense"))
+                  child: const Text("Save Expense")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel"))
             ],
           )
         ],
