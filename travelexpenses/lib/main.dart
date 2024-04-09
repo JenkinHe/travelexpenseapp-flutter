cfb4e52115b05e47c17c7e2efc5fffab809dd854
Jenkibear <jenkinwork123@gmail.com>
2024-04-09 20:07:37 +1000

    basic colour theme

--- travelexpenses/lib/main.dart
+++ travelexpenses/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:travelexpenses/widgets/expenses.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Expenses(),
-      ),
-    ));
-}
+var myLightColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 60, 60));
 
+var myDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 107, 0, 0));
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(colorScheme: myLightColorScheme),
+    home: const Expenses(),
+  ));
+}
